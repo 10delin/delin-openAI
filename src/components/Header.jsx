b9a850b50123c05c730eb0f2dd172eff134e83bf
Antonio Delage <delincruzado_20@hotmail.com>
2023-08-01 18:26:52 +0200

    feat(header): style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import iconApp from "../assets/iconApp.png";
 
 const StyledWrapper = styled.div`
   position: fixed;
@@ -7,17 +8,46 @@ const StyledWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   gap: 1rem;
   width: 100%;
-  background-color: #3d3d3d;
+  background-color: transparent;
+  border-bottom: 1px solid #353535;
+  box-shadow: 0 0 10px 0 rgba(83, 83, 83, 0.3);
   color: white;
   z-index: 3;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 1.2rem;
+  font-weight: 500;
+  text-align: center;
+  letter-spacing: 0.5px;
+  margin: 0;
+  padding: 20px;
+`;
+
+const StyledImg = styled.img`
+  width: 60px;
+  height: 55px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
 export const Header = () => {
   return (
     <StyledWrapper>
-      <h4>Header</h4>
+      <StyledTitle>Delin OpenAI</StyledTitle>
+      <StyledImg
+        src={iconApp}
+        alt="icon"
+        onClick={() => window.location.reload()}
+      />
+      <StyledTitle>Profile</StyledTitle>
     </StyledWrapper>
   );
 };
